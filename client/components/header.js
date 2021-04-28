64ccf5c556f7d3843d10c379bb6b330936554333
Max Smagin <Max@MacBook-Air-MS.local>
2021-04-28 17:56:06 +0300

    Homework 12weekgo-back

--- client/components/header.js
+++ client/components/header.js
@@ -6,7 +6,7 @@ const Header = (props) => {
     <div className="flex flex-col bg-indigo-800 text-white font-bold shadow-lg p-2 items-center justify-center w-screen">
       <div id="repository-name">{props.user}</div>
       <div className="flex justify-between w-full">
-        <div id="go back" className="border rounded py-1 px-2">
+        <div id="go-back" className="border rounded py-1 px-2">
           <Link to="/">Back</Link>
         </div>
         <div id="go-repository-list" className="border rounded py-1 px-2">
