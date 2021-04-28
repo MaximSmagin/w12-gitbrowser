cd002dc92225de5591fe56bedd8a41c5bd2927e8
Max Smagin <Max@MacBook-Air-MS.local>
2021-04-28 18:08:34 +0300

    Some fixes

--- client/components/header.js
+++ client/components/header.js
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom'
 
 const Header = (props) => {
   return (
-    <div className="flex flex-col bg-indigo-800 text-white font-bold shadow-lg p-2 items-center justify-center w-screen">
+    <div className="flex flex-col justify-center items-center bg-indigo-800 text-white font-bold shadow-lg p-2 w-screen">
       <div id="repository-name">{props.user}</div>
-      <div className="flex justify-between w-full">
+      <div className="flex justify-between w-full px-4">
         <div id="go-back" className="border rounded py-1 px-2">
           <Link to="/">Back</Link>
         </div>
